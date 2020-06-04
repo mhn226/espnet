2bc3595a77a575ffeb5fd1a2bdef793179d89696
mhn226 <mhn.22692@gmail.com>
2020-06-04 10:56:35 +0200

    Add trans_ensemble

--- espnet/st/pytorch_backend/trans.py
+++ espnet/st/pytorch_backend/trans.py
@@ -100,3 +100,102 @@ def trans(args):
 
     with open(args.result_label, 'wb') as f:
         f.write(json.dumps({'utts': new_js}, indent=4, ensure_ascii=False, sort_keys=True).encode('utf_8'))
+
+def trans_ensemble(args):
+    """Decode with custom models that implements ScorerInterface.
+    Notes:
+        The previous backend espnet.asr.pytorch_backend.st.trans only supports E2E and RNNLM
+    Args:
+        args (namespace): The program arguments. See py:func:`espnet.bin.st_trans.get_parser` for details
+    """
+    logging.warning("experimental API for custom LMs is selected by --api v2")
+    if args.batchsize > 1:
+        raise NotImplementedError("batch decoding is not implemented")
+    #if args.streaming_mode is not None:
+    #    raise NotImplementedError("streaming mode is not implemented")
+    #if args.word_rnnlm:
+    #    raise NotImplementedError("word LM is not implemented")
+
+    set_deterministic_pytorch(args)
+    models = []
+    train_args = []
+    for model_arg in args.model:
+        model, train_args_ = load_trained_model(model_arg)
+        assert isinstance(model, STInterface)
+        model.eval()
+        models.append(model)
+        train_args.append(train_args_)
+
+    prep_conf = train_args[0].preprocess_conf
+    char_list = train_args[0].char_list
+
+    load_inputs_and_targets = LoadInputsAndTargets(
+        mode='asr', load_output=False, sort_in_input_length=False,
+        preprocess_conf=prep_conf
+        if args.preprocess_conf is None else args.preprocess_conf,
+        preprocess_args={'train': False})
+
+    if args.rnnlm:
+        lm_args = get_model_conf(args.rnnlm, args.rnnlm_conf)
+        # NOTE: for a compatibility with less than 0.5.0 version models
+        lm_model_module = getattr(lm_args, "model_module", "default")
+        lm_class = dynamic_import_lm(lm_model_module, lm_args.backend)
+        lm = lm_class(len(char_list), lm_args)
+        torch_load(args.rnnlm, lm)
+        lm.eval()
+    else:
+        lm = None
+
+    scorers = []
+    for model in models:
+        model.scorers["lm"] = lm
+        model.scorers["length_bonus"] = LengthBonus(len(char_list))
+        scorers = model.scorers()
+    weights = dict(
+        decoder=1.0,
+        lm=args.lm_weight,
+        length_bonus=args.penalty)
+    beam_search = BeamSearch(
+        beam_size=args.beam_size,
+        vocab_size=len(char_list),
+        weights=weights,
+        scorers=scorers,
+        sos=model.sos,
+        eos=model.eos,
+        token_list=char_list,
+    )
+
+    if args.ngpu > 1:
+        raise NotImplementedError("only single GPU decoding is supported")
+    if args.ngpu == 1:
+        device = "cuda"
+    else:
+        device = "cpu"
+    dtype = getattr(torch, args.dtype)
+    logging.info(f"Decoding device={device}, dtype={dtype}")
+    for model in models:
+        model.to(device=device, dtype=dtype).eval()
+    beam_search.to(device=device, dtype=dtype).eval()
+
+    # read json data
+    js = []
+    for trans_json in args.trans_json:
+        with open(trans_json, 'rb') as f:
+            js_ = json.load(f)['utts']
+        js.append(js_)
+    new_js = {}
+    with torch.no_grad():
+        for idx, name in enumerate(js[0].keys(), 1):
+            logging.info('(%d/%d) decoding ' + name, idx, len(js[0].keys()))
+            encs = []
+            for js_ in js:
+                batch = [(name, js_[name])]
+                feat = load_inputs_and_targets(batch)[0][0]
+                enc = model.encode(torch.as_tensor(feat).to(device=device, dtype=dtype))
+                encs.append(enc)
+            nbest_hyps = beam_search(x=encs, maxlenratio=args.maxlenratio, minlenratio=args.minlenratio)
+            nbest_hyps = [h.asdict() for h in nbest_hyps[:min(len(nbest_hyps), args.nbest)]]
+            new_js[name] = add_results_to_json(js[name], nbest_hyps, char_list)
+
+    with open(args.result_label, 'wb') as f:
+        f.write(json.dumps({'utts': new_js}, indent=4, ensure_ascii=False, sort_keys=True).encode('utf_8'))
