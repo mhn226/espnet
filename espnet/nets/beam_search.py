016e8a5ad39781c4a584d5cb322547f3aed604dc
mhn226 <mhn.22692@gmail.com>
2020-06-04 19:52:33 +0200

    Update beam search to work with ensemble

--- espnet/nets/beam_search.py
+++ espnet/nets/beam_search.py
@@ -319,6 +319,7 @@ class BeamSearch(torch.nn.Module):
                         print(score_k)
                         full_avg_scores[k] = F.log_softmax(full_avg_scores[k], dim=1).squeeze()
                         print(full_avg_scores[k])
+                        aaaaaaaaaaaaa
                 for k in self.part_scorers[0]:
                     score_k = [score[k] for score in part_scores]
                     part_avg_scores[k] = torch.mean(torch.stack(score_k), dim=0)
