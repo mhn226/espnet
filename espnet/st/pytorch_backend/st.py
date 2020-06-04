2982273a30f2b0a7d44c9cff2889f40d613326b7
mhn226 <mhn.22692@gmail.com>
2020-06-04 21:54:52 +0200

    Update beam search

--- espnet/st/pytorch_backend/st.py
+++ espnet/st/pytorch_backend/st.py
@@ -562,6 +562,9 @@ def trans_step_ensemble_parallelizing(models, feat, rnnlm, train_args, enc_pool)
                                                         model.trans_args, train_args[model_index].char_list, rnnlm)
             logits = torch.mean(torch.stack(logits), dim=0)
             local_att_scores = F.log_softmax(logits, dim=1)
+            print("######### ", logits)
+            print("######### ", local_att_scores)
+            aaaaaaaaaaaaaaaaaaaaa
 
             if rnnlm:
                 # rnnlm_state, local_lm_scores = rnnlm.predict(hyp['rnnlm_prev'], vy)
@@ -713,9 +716,6 @@ def trans_step_ensemble(models, feat, rnnlm, train_args):
                                                         model.trans_args, train_args[model_index].char_list, rnnlm)
             logits = torch.mean(torch.stack(logits), dim=0)
             local_att_scores = F.log_softmax(logits, dim=1)
-            print("######### ", logits)
-            print("######### ", local_att_scores)
-            aaaaaaaaaaaaaaaaaaaaa
 
             if rnnlm:
                 # rnnlm_state, local_lm_scores = rnnlm.predict(hyp['rnnlm_prev'], vy)
