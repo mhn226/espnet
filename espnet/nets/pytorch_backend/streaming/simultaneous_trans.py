dfa8e194ed7bd991b4b29fb8c097c308e901f6f0
mhn226 <mhn.22692@gmail.com>
2020-06-22 10:39:53 +0200

    Add silmustaneous_trans.py, Update trans.py

---
+++ espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
@@ -0,0 +1,157 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+READ=0
+WRITE=1
+
+class SimultaneousSTE2E(object):
+    """SimultaneousSTE2E constructor.
+    :param E2E e2e: E2E ST object
+    :param trans_args: arguments for "trans" method of E2E
+    """
+
+    def __init__(self, e2e, trans_args, rnnlm=None):
+        self._e2e = e2e
+        self._trans_args = trans_args
+        self._char_list = e2e.char_list
+        self._rnnlm = rnnlm
+
+        self._e2e.eval()
+
+        self._blank_idx_in_char_list = -1
+        for idx in range(len(self._char_list)):
+            if self._char_list[idx] == self._e2e.blank:
+                self._blank_idx_in_char_list = idx
+                break
+
+        self._subsampling_factor = np.prod(e2e.subsample)
+        self._activates = 0
+        self._blank_dur = 0
+
+        self._previous_input = []
+        self._previous_encoder_recurrent_state = None
+        self._encoder_states = []
+        self._ctc_posteriors = []
+
+        self.enc_states = []
+        #self.c_list = []
+        #self.z_list = []
+        #self.a = []
+        #self.y = self._e2e.dec.sos
+
+        if self._trans_args.ngpu > 1:
+            raise NotImplementedError("only single GPU decoding is supported")
+        if self._trans_args.ngpu == 1:
+            self.device = "cuda"
+        else:
+            self.device = "cpu"
+        self.dtype = getattr(torch, self._trans_args.dtype)
+
+        #yseq = torch.tensor([self.sos], device=x[0].device))
+        self.hyp = {'score': 0.0, 'yseq': torch.tensor([self._e2e.dec.sos], device=self.device), 'c_prev': [], 'z_prev': [], 'a_prev': []}
+        self.finished = False
+        self.finish_read = False
+        self.last_action = None
+        self.frame_count = 0
+
+        assert self._trans_args.batchsize <= 1, \
+            "SegmentStreamingE2E works only with batch size <= 1"
+        assert "b" not in self._e2e.etype, \
+            "SegmentStreamingE2E works only with uni-directional encoders"
+
+    def decision_from_states(self):
+        #print('State\n|| Target:', ''.join(states['tokens']['tgt']), '\n|| ASR:', ''.join(states['tokens']['asr']))
+
+        if len(self.enc_states) == 0:
+            return READ
+
+        # follow every read with a transcription:
+        if self.last_action == READ:
+            return WRITE
+        else:
+            return READ
+
+        #return READ
+
+    def policy(self, x):
+        # Read and Write policy
+        action = None
+
+        while action is None:
+            if self.finished:
+                # Finish the hypo by sending eos to server
+                return self.finish_action()
+
+            # Model make decision given current states
+            decision = self.decision_from_states()
+
+            if decision == READ and not self.finish_read():
+                # READ
+                self.last_action = decision
+                action = self.read_action(x)
+
+            #elif decision == TRANSCRIBE and not self.finish_transcription(states):
+            #    # TRANSCRIBE
+            #    states['last_action'] = decision
+            #    action = self.transcribe_action(states)
+
+            else:
+                # WRITE
+                self.last_action = WRITE
+                action = self.write_action()
+
+            # None means we make decision again but not sending server anything
+            # This happened when read a bufffered token
+            # Or predict a subword
+        return action
+
+    def read_action(self, x):
+        # segment_size =  160000  # Wait-until-end
+        self.frame_count += 7000
+        # if states["steps"]["src"] == 0:
+        x = x[:self.frame_count]
+        h, ilen = self._e2e.subsample_frames(x)
+        # Run encoder and apply greedy search on CTC softmax output
+        self.enc_states = self._e2e.encode(torch.as_tensor(h).to(device=self.device, dtype=self.dtype))
+        #h, _, self._previous_encoder_recurrent_state = self._e2e.enc(
+        #    h.unsqueeze(0),
+        #    ilen,
+        #    self._previous_encoder_recurrent_state
+        #)
+        return {'key': 'GET', 'value': {'enc_states': self.enc_states}}
+        # segment_size = 1000 * 3
+        #return {'key': GET, 'value': {"segment_size": segment_size}}
+
+    def write_action(self):
+        model_index = 0
+
+        if self.hyp['yseq'][len(self.hyp['yseq'])-1] == self._e2e.dec.eos or len(self.hyp['yseq']) > self.max_len:
+            # Finish this sentence is predict EOS
+            self.finished = True
+
+        score, states = self._e2e.dec.score(self.hyp.yseq, self.hyp.states, self.enc_states)
+        score = F.log_softmax(score, dim=1).squeeze()
+        # greedy search, take only the (1) best score
+        local_best_score, local_best_id = torch.topk(score, 1, dim=1)
+
+        # [:] is needed!
+        self.hyp['z_prev'] = self.z_list[:]
+        self.hyp['c_prev'] = self.c_list[:]
+        self.hyp['a_prev'] = self.att_w[:]
+        self.hyp['score'] = self.hyp['score'] + local_best_score[0]
+        self.hyp['yseq'] = [0] * (1 + len(self.hyp['yseq']))
+        self.hyp['yseq'][:len(self.hyp['yseq'])] = self.hyp['yseq']
+        self.hyp['yseq'][len(self.hyp['yseq'])] = int(local_best_id[0])
+        #if rnnlm:
+        #    self.hyp['rnnlm_prev'] = rnnlm_state
+        # will be (2 x beam) hyps at most
+
+        return {'key': 'SEND', 'value': {'dec_hyp': self.hyp}}
+
+    def finish_read(self):
+        return self.finish_read
+
+    def finish_action(self):
+        return {'key': 'SEND', 'value': {'dec_hyp': self._e2e.dec.eos}}
+
--- espnet/st/pytorch_backend/trans.py
+++ espnet/st/pytorch_backend/trans.py
@@ -9,6 +9,7 @@ from espnet.asr.asr_utils import add_results_to_json
 from espnet.asr.asr_utils import get_model_conf
 from espnet.asr.asr_utils import torch_load
 from espnet.asr.pytorch_backend.asr_init import load_trained_model
+from espnet.nets.pytorch_backend.streaming.simultaneous_trans import SimultaneousSTE2E
 from espnet.nets.st_interface import STInterface
 from espnet.nets.beam_search import BeamSearch
 from espnet.nets.lm_interface import dynamic_import_lm
@@ -112,6 +113,118 @@ def trans(args):
             nbest_hyps = beam_search(x=encs, maxlenratio=args.maxlenratio, minlenratio=args.minlenratio)
             nbest_hyps = [h.asdict() for h in nbest_hyps[:min(len(nbest_hyps), args.nbest)]]
             new_js[name] = add_results_to_json(js[0][name], nbest_hyps, char_list)
+
+    with open(args.result_label, 'wb') as f:
+        f.write(json.dumps({'utts': new_js}, indent=4, ensure_ascii=False, sort_keys=True).encode('utf_8'))
+
+
+def trans_waitk(args):
+    """Decode with custom models that implements ScorerInterface.
+    Notes:
+        The previous backend espnet.asr.pytorch_backend.st.trans only supports E2E and RNNLM
+    Args:
+        args (namespace): The program arguments. See py:func:`espnet.bin.st_trans.get_parser` for details
+    """
+    logging.warning("experimental API for custom LMs is selected by --api v2")
+    if args.batchsize > 1:
+        raise NotImplementedError("batch decoding is not implemented")
+    #if args.streaming_mode is not None:
+    #    raise NotImplementedError("streaming mode is not implemented")
+    #if args.word_rnnlm:
+    #    raise NotImplementedError("word LM is not implemented")
+
+    set_deterministic_pytorch(args)
+    model, train_args = load_trained_model(args.model)
+    assert isinstance(model, STInterface)
+    model.eval()
+
+    load_inputs_and_targets = LoadInputsAndTargets(
+        mode='asr', load_output=False, sort_in_input_length=False,
+        preprocess_conf=train_args.preprocess_conf
+        if args.preprocess_conf is None else args.preprocess_conf,
+        preprocess_args={'train': False})
+
+    if args.rnnlm:
+        lm_args = get_model_conf(args.rnnlm, args.rnnlm_conf)
+        # NOTE: for a compatibility with less than 0.5.0 version models
+        lm_model_module = getattr(lm_args, "model_module", "default")
+        lm_class = dynamic_import_lm(lm_model_module, lm_args.backend)
+        lm = lm_class(len(train_args.char_list), lm_args)
+        torch_load(args.rnnlm, lm)
+        lm.eval()
+    else:
+        lm = None
+
+    scorers = model.scorers()
+    scorers["lm"] = lm
+    scorers["length_bonus"] = LengthBonus(len(train_args.char_list))
+    weights = dict(
+        decoder=1.0,
+        lm=args.lm_weight,
+        length_bonus=args.penalty)
+    beam_search = BeamSearch(
+        beam_size=args.beam_size,
+        vocab_size=len(train_args.char_list),
+        weights=weights,
+        scorers=scorers,
+        sos=model.sos,
+        eos=model.eos,
+        token_list=train_args.char_list,
+    )
+
+    if args.ngpu > 1:
+        raise NotImplementedError("only single GPU decoding is supported")
+    if args.ngpu == 1:
+        device = "cuda"
+    else:
+        device = "cpu"
+    dtype = getattr(torch, args.dtype)
+    logging.info(f"Decoding device={device}, dtype={dtype}")
+    model.to(device=device, dtype=dtype).eval()
+    beam_search.to(device=device, dtype=dtype).eval()
+
+    # read json data
+    with open(args.trans_json, 'rb') as f:
+        js = json.load(f)['utts']
+    new_js = {}
+    with torch.no_grad():
+        for idx, name in enumerate(js.keys(), 1):
+            logging.info('(%d/%d) decoding ' + name, idx, len(js.keys()))
+            batch = [(name, js[name])]
+            k = 100 # 100 frames = 1s
+            g = 4 # 4 frames at a time
+            feat = load_inputs_and_targets(batch)[0][0]
+            #se2e = SimultaneousSTE2E(e2e=model, recog_args=args, rnnlm=rnnlm)
+            se2e = SimultaneousSTE2E(e2e=model, recog_args=args)
+            action = {}
+            nbest_hyps = []
+            for n in range(args.nbest):
+                nbest_hyps.append({"yseq": [], "score": 0.0})
+
+            while action.get('value', None) != model.dec.eos:
+                # take an action
+                action = se2e.policy(feat)
+
+                if action['key'] == 'GET':
+                    print('get')
+                    #new_states = session.get_src(sent_id, action["value"])
+                    #states = self.update_states(states, new_states)
+
+                elif action['key'] == 'SEND':
+                    text = "".join(
+                        [
+                            train_args.char_list[int(x)]
+                            for x in action['value']['dec_hyp']['yseq'][1:-1]
+                            if int(x) != -1
+                        ]
+                    )
+                    logging.info(text)
+                    for n in range(args.nbest):
+                        nbest_hyps[n]['yseq'].extend(action['value']['dec_hyp']['yseq'])
+                        nbest_hyps[n]['score'] += action['value']['dec_hyp']['score']
+            #nbest_hyps = [h.asdict() for h in nbest_hyps[:min(len(nbest_hyps), args.nbest)]]
+            nbest_hyps = [nbest_hyps]
+            new_js[name] = add_results_to_json(js[name], nbest_hyps, train_args.char_list)
 
     with open(args.result_label, 'wb') as f:
         f.write(json.dumps({'utts': new_js}, indent=4, ensure_ascii=False, sort_keys=True).encode('utf_8'))
