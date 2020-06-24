d5c6d986eba8d82a1eb66628c888bfbf740de5da
mhn226 <mhn.22692@gmail.com>
2020-06-24 10:46:13 +0200

    Add log to simultaneous_trans.py

--- espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
+++ espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
@@ -154,10 +154,12 @@ class SimultaneousSTE2E(object):
         score = F.log_softmax(score, dim=1).squeeze()
         # greedy search, take only the (1) best score
         local_best_score, local_best_id = torch.topk(score, 1)
+        logging.info(local_best_score)
         if not self.finish_read and int(local_best_id) == self._e2e.dec.eos:
             tmp_scores, tmp_ids = torch.topk(score, 2)
             local_best_score = [tmp_scores[1]]
             local_best_id = [tmp_ids[1]]
+            logging.info(local_best_score)
             logging.info('EOS emits before reading all of source frames, choose the second best target token instead: ' + str(local_best_id) + ', ' + self._char_list[local_best_id])
 
         # [:] is needed!
