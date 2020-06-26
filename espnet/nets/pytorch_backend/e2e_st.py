3071eb2d7c56e5712fe619f742f3fb781831c51a
mhn226 <mhn.22692@gmail.com>
2020-06-27 01:02:49 +0200

    update simul training

--- espnet/nets/pytorch_backend/e2e_st.py
+++ espnet/nets/pytorch_backend/e2e_st.py
@@ -381,7 +381,7 @@ class E2E(STInterface, torch.nn.Module):
             else:
                 xs_pad_ = xs_pad[:self.g]
                 ilens_ = torch.zeros(ilens.size(), dtype=ilens.dtype, device=ilens.device)
-                ilens_ = ilens_.new_full(ilens_.size(), fill_value=self.g)
+                ilens_ = ilens_.new_full(ilens.size(), fill_value=self.g)
             print(ilens_.size())
             hs_pad, hlens, _ = self.enc(xs_pad_, ilens_)
             if self.g == self.k:
