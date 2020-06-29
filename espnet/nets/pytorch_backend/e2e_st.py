68db61e1f1ab1df36746cab3e532fd864ef90a21
mhn226 <mhn.22692@gmail.com>
2020-06-29 16:00:04 +0200

    ,Up date simul training

--- espnet/nets/pytorch_backend/e2e_st.py
+++ espnet/nets/pytorch_backend/e2e_st.py
@@ -670,7 +670,10 @@ class E2E(STInterface, torch.nn.Module):
                 #y_true = ys_pad[i]
                 print('y_hat: ', y_hat, y_hat.size())
                 #print('y_true: ', y_true)
+                y_hat = y_hat.detach().cpu().numpy()
                 y_true = ys_out_pad[i]
+                print('y_true: ', y_true.size())
+                y_true = y_true.detach().cpu().numpy()
 
                 idx_hat = np.argmax(y_hat[y_true != self.dec.ignore_id], axis=1)
                 idx_true = y_true[y_true != self.dec.ignore_id]
