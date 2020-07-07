9e9dc05ad4573898ada1b749c7e43fb405b21e3b
mhn226 <mhn.22692@gmail.com>
2020-07-07 12:19:54 +0200

    add single direction lstm simultaneous trans

--- espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
+++ espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
@@ -132,14 +132,16 @@ class SimultaneousSTE2E(object):
         self.offset = self.g
         self.g += self.s
         if len(self.enc_states) == 0:
-            self.enc_states = torch.empty((1, 0, h.size(2)), device=self.device)
+            self.enc_states = [torch.empty((1, 0, h[0].size(2)), device=self.device)]
         #if self.dec.num_encs == 1:
         #    hs_pad = [hs_pad]
         #    hlens = [hlens]
         #hlens = [list(map(int, hlens[idx])) for idx in range(self.dec.num_encs)]
 
         #self.enc_states.append(h)
-        self.enc_states = torch.cat((self.enc_states, h), dim=1)
+        print(h.size(2))
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        self.enc_states[0] = torch.cat((self.enc_states[0], h[0]), dim=1)
 
         #return hs_pad, hlens, last_enc_states, finished_read
 
