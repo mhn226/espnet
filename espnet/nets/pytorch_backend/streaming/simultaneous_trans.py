8a55f8ca3f38370709709cccf87a3a8b3d7673f3
mhn226 <mhn.22692@gmail.com>
2020-07-15 15:02:05 +0200

    add predefined policy

--- espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
+++ espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
@@ -108,7 +108,7 @@ class SimultaneousSTE2E(object):
         If a forced-aligment file is available, one could use it
         """
         segment_step = 0
-        segments = read_textgrid(segment_file, 5)
+        segments = read_textgrid(segment_file, k=5)
         for i, segment in enumerate(segments):
             if segment[1] >= self.g:
                 self.g = segment[1]
