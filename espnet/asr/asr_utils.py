9811b32f805dc304e9c588f4f4ac788ae7b38307
mhn226 <mhn.22692@gmail.com>
2020-07-27 16:24:48 +0200

    add predefined policy

--- espnet/asr/asr_utils.py
+++ espnet/asr/asr_utils.py
@@ -544,6 +544,7 @@ def torch_resume(snapshot_path, trainer):
     if "ctc.ctc_lo.weight" in snapshot_dict['model']:
         del snapshot_dict['model']["ctc.ctc_lo.weight"]
         del snapshot_dict['model']["ctc.ctc_lo.bias"]
+        #del snapshot_dict['optimizer'][50:52]
 
     # restore trainer states
     d = NpzDeserializer(snapshot_dict['trainer'])
