f7c6806d476b1b027dd765e9341920929d825d4d
mhn226 <mhn.22692@gmail.com>
2020-10-02 18:02:32 +0200

    Update simul trans

--- espnet/st/pytorch_backend/trans.py
+++ espnet/st/pytorch_backend/trans.py
@@ -248,7 +248,7 @@ def trans_waitk(args):
             nbest_hyps[0]['scrore'] = action['value']['dec_hyp']['score']
             logging.info('delays: ' + str(action['value']['dec_hyp']['delays']))
             latency = eval_all_latency(action['value']['dec_hyp']['delays'], js[name]['input'][0]['shape'][0])
-            nbest_hyps[0]['delays'] = latency
+            nbest_hyps[0]['latency'] = latency
             new_js[name] = add_results_to_json(js[name], nbest_hyps, train_args.char_list)
             logging.info('latency: ' + str(latency))
 
