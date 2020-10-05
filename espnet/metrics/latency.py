fa2557f0f5da24f821f23f42d2a61e758a28b33e
mhn226 <mhn.22692@gmail.com>
2020-10-05 14:07:10 +0200

    Update simul trans

--- espnet/metrics/latency.py
+++ espnet/metrics/latency.py
@@ -32,7 +32,7 @@ def latency_metric(func):
             src_lens = torch.FloatTensor([src_lens])
 
         if isinstance(target_padding_mask, numbers.Number):
-            src_lens = torch.FloatTensor([target_padding_mask])
+            target_padding_mask = torch.FloatTensor([target_padding_mask])
 
         src_lens = src_lens.type_as(delays)
 
