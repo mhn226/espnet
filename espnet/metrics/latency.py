9fc6ab46424c805a75b4fe00fb6b4e98014329d3
mhn226 <mhn.22692@gmail.com>
2020-10-05 14:35:12 +0200

    Update metrics

--- espnet/metrics/latency.py
+++ espnet/metrics/latency.py
@@ -23,8 +23,6 @@ def latency_metric(func):
             delays = torch.FloatTensor(delays)
 
         if len(delays.size()) == 1:
-            print(delays)
-            aaaaaaaaaaaaaaaaaaaaaaaaaa
             delays = delays.view(1, -1)
 
         if isinstance(src_lens, list):
