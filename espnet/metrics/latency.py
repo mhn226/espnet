c150547b05ad0a6b8cd21a0e12b5be9d18d19577
mhn226 <mhn.22692@gmail.com>
2020-10-05 10:19:15 +0200

    Update simul trans

--- espnet/metrics/latency.py
+++ espnet/metrics/latency.py
@@ -129,7 +129,7 @@ def DifferentiableAverageLagging(
                     delays[:, i].unsqueeze(1)
                 ],
                 dim=1
-            ).max(dim=1))
+            ).max(dim=1).values)
             new_delays[:, i] = torch.cat(
                 [
                     new_delays[:, i - 1].unsqueeze(1) + 1 / gamma,
