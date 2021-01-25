6c6cafe806f045b17b6ae28bdec8b5a1daf416f2
mhn226 <mhn.22692@gmail.com>
2021-01-25 19:14:01 +0100

    Update simul train

--- espnet/nets/pytorch_backend/rnn/encoders.py
+++ espnet/nets/pytorch_backend/rnn/encoders.py
@@ -312,9 +312,6 @@ class Encoder(torch.nn.Module):
             for module, prev_state in zip(self.enc, prev_states):
                 xs_pad_, ilens_, states = module(xs_pad_, ilens_, prev_state=prev_state)
                 print('x_pad_ encoded', xs_pad_.size(), ilens_[0])
-                if states is not None:
-                    for i, state in enumerate(states):
-                        print('state ' + str(i) + ': ', state.size())
                 current_states_.append(states)
 
             # make mask to remove bias value in padded part
