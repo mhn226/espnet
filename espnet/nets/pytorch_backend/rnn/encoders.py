9dae6379f413d3d6bfd5c1ef924dc784e828ab71
mhn226 <mhn.22692@gmail.com>
2021-01-25 17:34:42 +0100

    Update simul train

--- espnet/nets/pytorch_backend/rnn/encoders.py
+++ espnet/nets/pytorch_backend/rnn/encoders.py
@@ -303,7 +303,7 @@ class Encoder(torch.nn.Module):
                 xs_pad_ = xs_pad.transpose(1, 2)[:, :, offset:g].transpose(1, 2)
                 ilens_ = torch.zeros(ilens.size(), dtype=ilens.dtype, device=ilens.device)
                 ilens_ = ilens_.new_full(ilens.size(), fill_value=(g - offset))
-                print(xs_pad_.size(), ilens_.size(), xs_pad.transpose(0, 1).clone[offset:g].size())
+                print(xs_pad_.size(), ilens_.size(), xs_pad.transpose(0, 1).clone()[offset:g].size())
 
             assert len(prev_states) == len(self.enc)
 
