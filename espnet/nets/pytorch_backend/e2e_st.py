0051de22c18c85942008205d2cadf181ce435151
mhn226 <mhn.22692@gmail.com>
2021-01-26 16:49:34 +0100

    Update simul train

--- espnet/nets/pytorch_backend/e2e_st.py
+++ espnet/nets/pytorch_backend/e2e_st.py
@@ -452,6 +452,7 @@ class E2E(STInterface, torch.nn.Module):
                 #offset = g
                 print(torch.cuda.memory_allocated() / torch.cuda.max_memory_allocated())
                 hs_pad, hlens, finished_read = self.action_read(xs_pad, ilens, g, finished_read)
+                g += s
                 continue
                 z_list, c_list, att_w, z_ = self.dec(hs_pad, hlens, dec_step, att_idx, z_list, c_list, att_w, z_all, eys)
                 #z_all.append(z_.detach())
