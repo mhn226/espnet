850fe35b7396845c08335b15ee5716520c47d19f
mhn226 <mhn.22692@gmail.com>
2021-01-29 12:12:22 +0100

    Update simul train

--- espnet/nets/pytorch_backend/e2e_st.py
+++ espnet/nets/pytorch_backend/e2e_st.py
@@ -466,9 +466,9 @@ class E2E(STInterface, torch.nn.Module):
                 z_list, c_list, att_w, z_ = self.dec(hs_pad, hlens, dec_step, att_idx, z_list, c_list, att_w, z_all, olength-dec_step, eys)
                 z_all.extend(z_)
                 print(len(z_all), len(z_))
-            z_ = torch.stack(z_, dim=1)
-            print(z_.size(), torch.stack(z_all, dim=1).size())
-            z_all = torch.stack(z_all, dim=1).view(batch * olength, -1)
+            z_all = torch.stack(z_all, dim=1)
+            print(z_all.size())
+            z_all = z_all.view(batch * olength, -1)
             print(z_all.size())
             # compute loss
             y_all = self.dec.output(z_all)
