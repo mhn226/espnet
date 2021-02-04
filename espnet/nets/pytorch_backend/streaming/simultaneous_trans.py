d53cb48edeba1694dc8f69fa6567624304014ec5
mhn226 <mhn.22692@gmail.com>
2021-02-04 16:41:25 +0100

    Update simul train

--- espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
+++ espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
@@ -362,9 +362,10 @@ class SimultaneousSTE2E(object):
         logging.info('frame_count=' + str(self.g))
         logging.info('len_in=' + str(len(x)))
         logging.info('enc_step: ' + str(segment_step))
-        if self.max_len < int(x.size(0) / 4):
+        print(len(x))
+        if self.max_len < int(len(x) / 4):
             print(self.max_len)
-            self.max_len = int(x.size(0) / 4)
+            self.max_len = int(len(x) / 4)
             print(self.max_len)
             aaaaaaaaaaaaaaaaaaaaaaaaaaaa
         if (self.g >= len(x)) or (segments is not None and segment_step >= len(segments)-1):
