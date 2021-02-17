ef916969a20c5f39e506f9fc7ec8f82647b38842
mhn226 <mhn.22692@gmail.com>
2021-02-17 20:11:56 +0100

    Update simul train

--- espnet/nets/pytorch_backend/rnn/encoders.py
+++ espnet/nets/pytorch_backend/rnn/encoders.py
@@ -184,11 +184,13 @@ class VGG2L(torch.nn.Module):
         print(xs_pad.size())
         xs_pad = F.max_pool2d(xs_pad, 2, stride=2, ceil_mode=True)
         print(xs_pad.size())
-        aaaaaaaaaa
 
         xs_pad = F.relu(self.conv2_1(xs_pad))
+        print(xs_pad.size())
         xs_pad = F.relu(self.conv2_2(xs_pad))
+        print(xs_pad.size())
         xs_pad = F.max_pool2d(xs_pad, 2, stride=2, ceil_mode=True)
+        print(xs_pad.size())
         if torch.is_tensor(ilens):
             ilens = ilens.cpu().numpy()
         else:
