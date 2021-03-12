e04dae904f8be274a1a8824e80d7b66d951aa82c
mhn226 <mhn.22692@gmail.com>
2021-03-12 09:18:37 +0100

    Update simul train

--- espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
+++ espnet/nets/pytorch_backend/streaming/simultaneous_trans.py
@@ -280,7 +280,7 @@ class SimultaneousSTE2E(object):
         segment_step = 0
         #segments = read_textgrid(segment_file, k=10)
         segments = self.read_textgrid(segment_file)
-        segments = rand_segs3(segments, self.k, 10, 50)
+        #segments = rand_segs3(segments, self.k, 10, 50)
         logging.info('pref segments: ' + str(segments))
         #segments = read_textgrid2(segment_file, k=5)
         #self.min_len = num_of_toks
